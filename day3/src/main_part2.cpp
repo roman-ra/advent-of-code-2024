49d7970714e6899efffa38e500a51c94709595c3
Roman Rakhmatullin <roman.rakhmatullin@posteo.net>
2024-12-03 19:27:02 +0100

    Redo Day 3 part 2 with one regex for all instructions and stateful parsing of the result

--- day3/src/main_part2.cpp
+++ day3/src/main_part2.cpp
@@ -12,29 +12,40 @@ int main(int argc, char* argv[])
 
     std::ifstream in{argv[1]};
     
-    std::regex mul_instruction_regex{"mul\\(([0-9]{1,3}),([0-9]{1,3})\\)"};
+    std::regex instruction_regex{"mul\\(([0-9]{1,3}),([0-9]{1,3})\\)|do\\(\\)|don't\\(\\)"};
     
     std::string line, merged;
     while (std::getline(in, line)) {
         merged += (line + "#");
     }
-    line += "do()"; // guaranteed do() at the end
-
-    size_t dont_pos = merged.find("don't()");
-    while (dont_pos != std::string::npos) {
-        size_t do_pos = merged.find("do()", dont_pos);
-        merged.erase(dont_pos, do_pos - dont_pos + 1);
-        dont_pos = merged.find("don't()");
-    }
 
     uint64_t sum{0u};
-    auto instructions_begin = std::sregex_iterator(merged.begin(), merged.end(), mul_instruction_regex);
+    auto instructions_begin = std::sregex_iterator(merged.begin(), merged.end(), instruction_regex);
     auto instructions_end = std::sregex_iterator();
+    bool doing = true;
     for (std::sregex_iterator i = instructions_begin; i != instructions_end; ++i)
     {
         std::smatch match = *i;
-        uint32_t arg1 = std::stoul(match[1].str()), arg2 = std::stoul(match[2].str());
-        sum += (arg1 * arg2);
+        switch (match[0].str()[0])
+        {
+        case 'm':
+            /* mul */
+            if (doing) {
+                uint32_t arg1 = std::stoul(match[1].str()), arg2 = std::stoul(match[2].str());
+                sum += (arg1 * arg2);
+            }
+            break;
+        case 'd':
+            if (match[0].str()[2] == 'n') {
+                /* don't */
+                doing = false;
+            } else {
+                doing = true;
+            }
+            break;
+        default:
+            break;
+        }
     }
 
     printf("Sum = %llu", sum);
